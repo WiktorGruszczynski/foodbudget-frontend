d361055299dfaa5e0a1ffd56d6a6fe2662298592
WiktorGruszczynski <wgwiktorwg@gmail.com>
2026-04-07 22:32:41 +0200

    feat: api url from env

--- src/services/api/requests.ts
+++ src/services/api/requests.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosResponse } from "axios";
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
