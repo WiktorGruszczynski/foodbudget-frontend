99a598c484c7b5b9bee094802dee55dab2195d2c
WiktorGruszczynski <wgwiktorwg@gmail.com>
2026-04-07 17:29:50 +0200

    fix: improve code quality

--- src/App.tsx
+++ src/App.tsx
@@ -112,7 +112,6 @@ function App() {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-        <footer>Footer</footer>
       </BrowserRouter>
     </div>
   )
--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -19,8 +19,15 @@ export default function RecipePage() {
     const [selectedOption, setSelectedOption] = useState(options[0])
 
     return (
-        <div className="recipe-cotainer">
-            <ul className="recipe-options">
+        <div className="recipe-container flex flex-col h-full md:flex-row md:gap-8 md:h-[calc(100vh-38px)]">
+            <ul className="
+                    /* MOBILKA: Wyskakuje nad navbar na dole */
+                    fixed bottom-9.5 left-0 w-full z-40
+                    flex flex-row justify-around bg-white border-t p-2 shadow-lg
+                    
+                    /* PC: Grzecznie wraca do kontenera po lewej */
+                    md:static md:w-64 md:flex-col md:justify-start md:border-t-0 md:shadow-none md:p-0
+                ">
                 {options.map(option => (
                     <li className={selectedOption===option?'active':''} onClick={() => setSelectedOption(option)} key={option}>
                         {option}
